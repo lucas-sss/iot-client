c7804d04f6f22b529618257b1315db0b25e23829
liuwei <liuwei@flksec.com>
2018-12-01 17:10:24 +0800

    初始化

---
+++ src/main/java/com/example/iotclienttest/IotClientTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.iotclienttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotClientTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotClientTestApplication.class, args);
+    }
+}
